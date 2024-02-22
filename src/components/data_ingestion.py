d0e6b53c9678f89a875f5a2f6b2a712d64427560
DevanshSingh17 <devansh.shivdas.tomar@gmail.com>
2024-02-22 11:03:35 +0530

    data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,8 +6,10 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from dataclasses import dataclass
 
-
+from src.components.data_transformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformation
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join("artifacts","train.csv")
@@ -17,6 +19,7 @@ class DataIngestionConfig:
 class DataIngetion:
     def __init__(self) :
         self.ingestion_config=DataIngestionConfig()
+        
     def initiate_data_ingestion(self):
         logging.info("enter the data ingestion method or component")
         try:
@@ -43,7 +46,12 @@ class DataIngetion:
         
 if __name__=="__main__":
     obj=DataIngetion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
+    
+    
+    
     
     
             
